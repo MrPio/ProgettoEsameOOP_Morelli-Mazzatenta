5ef7d3763ee25840d4cbdb35fa35baaed5af113e
Valerio Morelli <valeriomorelli50@gmail.com>
2021-12-12 19:40:19 +0100

    folder fix

--- src/main/java/com/univpm/po/NutritionStats/Model/User.java
+++ src/main/java/com/univpm/po/NutritionStats/Model/User.java
@@ -1,106 +1,76 @@
-package com.univpm.po.NutritionStats.Model;
+package com.univpm.po.NutritionStats.model;
+
+import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import java.util.HashMap;
 
-public class User {
-	
-	private String name;
-	private String surname;
-	private String nickname;
-	private String email;
-	private String password;
-	private int yearOfBirth;
-	private HashMap <LocalDate,String> height = new HashMap<LocalDate,String>();
-	private HashMap <LocalDate,String> weight = new HashMap<LocalDate,String>();
-	private boolean vegetarian;
-	private boolean vegan;
-	private boolean celiac;
-	
-	public User(String name, String surname, String nickname, String email, String password, int yearOfBirth,
-			HashMap<LocalDate, String> height, HashMap<LocalDate, String> weight, boolean vegetarian, boolean vegan,
-			boolean celiac) {
-		this.name = name;
-		this.surname = surname;
-		this.nickname = nickname;
-		this.email = email;
-		this.password = password;
-		this.yearOfBirth = yearOfBirth;
-		this.height = height;
-		this.weight = weight;
-		this.vegetarian = vegetarian;
-		this.vegan = vegan;
-		this.celiac = celiac;
-	}
+public class User implements Serializable {
+    public enum Diet{CLASSIC,PESCATARIAN,VEGETARIAN,VEGAN};
+    public enum Gender{MALE,FEMALE};
 
-	public String getSurname() {
-		return surname;
-	}
+    private String nickname;
+    private String email;
+    private Diet diet;
+    private Gender gender;
+    private int yearOfBirth;
+    private HashMap<LocalDate, Integer> height = new HashMap<>();
+    private HashMap<LocalDate, Integer> weight = new HashMap<>();
+    //private boolean celiac;
 
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
+    public User(String email) {
+        this.email = email;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public User(String nickname, String email, int yearOfBirth,
+                int height, int weight, Diet diet,Gender gender) {
+        this.nickname = nickname;
+        this.email = email;
+        this.yearOfBirth = yearOfBirth;
+        this.height.put(LocalDate.now(),height);
+        this.weight.put(LocalDate.now(),weight);
+        this.diet=diet;
+        this.gender=gender;
+    }
 
-	public HashMap<LocalDate, String> getHeight() {
-		return height;
-	}
+    public String getNickname() {
+        return nickname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+    public HashMap<LocalDate, Integer> getHeight() {
+        return height;
+    }
+    public HashMap<LocalDate, Integer> getWeight() {
+        return weight;
+    }
+    public Diet getDiet() {
+        return diet;
+    }
+    public Gender getGender() {
+        return gender;
+    }
 
-	public void setHeight(HashMap<LocalDate, String> height) {
-		this.height = height;
-	}
-
-	public HashMap<LocalDate, String> getWeight() {
-		return weight;
-	}
-
-	public void setWeight(HashMap<LocalDate, String> weight) {
-		this.weight = weight;
-	}
-
-	public boolean isVegetarian() {
-		return vegetarian;
-	}
-
-	public void setVegetarian(boolean vegetarian) {
-		this.vegetarian = vegetarian;
-	}
-
-	public boolean isVegan() {
-		return vegan;
-	}
-
-	public void setVegan(boolean vegan) {
-		this.vegan = vegan;
-	}
-
-	public boolean isCeliac() {
-		return celiac;
-	}
-
-	public void setCeliac(boolean celiac) {
-		this.celiac = celiac;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getNickname() {
-		return nickname;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public int getYearOfBirth() {
-		return yearOfBirth;
-	}
-	
-	
-	
-	
+    public String generateToken() {
+        byte[] bytesOfMessage = new byte[0];
+        try {
+            bytesOfMessage = email.getBytes(StandardCharsets.UTF_8);
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] theMD5digest = md.digest(bytesOfMessage);
+            StringBuffer sb = new StringBuffer();
+            for (byte b : theMD5digest) {
+                sb.append(Integer.toHexString((b & 0xFF) | 0x100), 1, 3);
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
