075d1c54df433ad5797343d54cf00c6da3d003c9
Paccise <mazzada2001@gmail.com>
2021-12-13 17:48:35 +0100

    Update Water.java

--- src/main/java/com/univpm/po/NutritionStats/model/Water.java
+++ src/main/java/com/univpm/po/NutritionStats/model/Water.java
@@ -1,7 +1,17 @@
 package com.univpm.po.NutritionStats.model;
 
 public class Water {
-	public enum portionType{Glass,Bottle500ml,Bottle1000ml};
+	public enum portionType {
+		Glass(200),
+		Cup(350),
+		Bottle500ml(500),
+		Bottle1000ml(1000);
+		public int value;
+
+		private portionType(int value) {
+			this.value = value;
+		}
+};
 	private int portionVolume;
 	
 	public Water(int portionVolume) {
