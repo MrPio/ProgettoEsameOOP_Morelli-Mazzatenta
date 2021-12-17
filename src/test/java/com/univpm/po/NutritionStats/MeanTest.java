8ed96745681c075de7ec1a98c0e57d11caf09b1d
Paccise <mazzada2001@gmail.com>
2021-12-17 19:33:18 +0100

    quick fix

--- src/test/java/com/univpm/po/NutritionStats/MeanTest.java
+++ src/test/java/com/univpm/po/NutritionStats/MeanTest.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 
+import com.univpm.po.NutritionStats.exception.EndDateBeforeStartDateException;
 import com.univpm.po.NutritionStats.model.Diary;
 import com.univpm.po.NutritionStats.service.Mean;
 
@@ -12,7 +13,7 @@ class MeanTest {
 	final String email = "email@example.com";
 
 	@Test
-	void testWithDebugger() {
+	void testWithDebugger() throws EndDateBeforeStartDateException {
 
 		LocalDate begin = LocalDate.now();
 		LocalDate end = LocalDate.now();
